223caf5aa993a30c4d16eb13e477aa3c668fab87
Yagiz Nizipli <yagiz@nizipli.com>
2022-09-03 17:43:46 -0400

    fix: simplify implementation and use node:qs tests

--- test/basics.test.ts
+++ test/basics.test.ts
@@ -1,42 +1,25 @@
 import qs from "../lib";
 import { test, assert } from "vitest";
+import vm from 'node:vm'
 
-test("parse single key value pair", () => {
-  assert.deepEqual(qs.parse("a=s"), { a: "s" });
-});
+const foreignObject = vm.runInNewContext('({"foo": ["bar", "baz"]})');
+const qsNoMungeTestCases = [
+  ['', {}],
+  ['foo=bar&foo=baz', { 'foo': ['bar', 'baz'] }],
+  ['foo=bar&foo=baz', foreignObject],
+  ['blah=burp', { 'blah': 'burp' }],
+  ['a=!-._~\'()*', { 'a': '!-._~\'()*' }],
+  ['a=abcdefghijklmnopqrstuvwxyz', { 'a': 'abcdefghijklmnopqrstuvwxyz' }],
+  ['a=ABCDEFGHIJKLMNOPQRSTUVWXYZ', { 'a': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' }],
+  ['a=0123456789', { 'a': '0123456789' }],
+  ['gragh=1&gragh=3&goo=2', { 'gragh': ['1', '3'], 'goo': '2' }],
+  ['frappucino=muffin&goat%5B%5D=scone&pond=moose',
+   { 'frappucino': 'muffin', 'goat[]': 'scone', 'pond': 'moose' }],
+  ['trololol=yes&lololo=no', { 'trololol': 'yes', 'lololo': 'no' }],
+];
 
-test("parse multiple key value pair", () => {
-  assert.deepEqual(qs.parse("hello=world&foo=bar"), {
-    hello: "world",
-    foo: "bar",
-  });
-});
-
-test("should create an array with multiple same keys", () => {
-  assert.deepEqual(qs.parse(
-    "language=javascript&language=typescript&sort=true",
-  ), {
-    language: ["javascript", "typescript"],
-    sort: "true",
-  });
-});
-
-test("should parse [] notation", () => {
-  assert.deepEqual(qs.parse("a[]=b&a[]=c"), {
-    a: ["b", "c"],
-  });
-});
-
-test("should parse single [] notation", () => {
-  assert.deepEqual(qs.parse("a[]=b"), {
-    a: ["b"],
-  });
-});
-
-test("parse nested key value pairs", () => {
-  assert.deepEqual(qs.parse("foo[bar]=baz"), {
-    foo: {
-      bar: "baz",
-    },
-  });
+test("should parse the basics", () => {
+  qsNoMungeTestCases.forEach((t) => {
+    assert.deepEqual(qs.parse(t[0]), t[1])
+  })
 });
