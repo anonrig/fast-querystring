1e8498e2d642ae6f52dcfbd40c5211b03759d966
Yagiz Nizipli <yagiz@nizipli.com>
2022-09-03 18:40:34 -0400

    fix: remove url-state-machine from benchmarks

--- test/benchmark.mjs
+++ test/benchmark.mjs
@@ -1,5 +1,4 @@
 import benchmark from "cronometro";
-import URLStateMachine from "url-state-machine";
 import qs from "qs";
 import fastQueryString from "../lib/index.js";
 import native from "node:querystring";
@@ -7,9 +6,6 @@ import queryString from "query-string";
 
 await benchmark(
   {
-    URLStateMachine() {
-      return new URLStateMachine("hello=world&foo=bar", undefined, null, 115);
-    },
     qs() {
       return qs.parse("hello=world&foo=bar");
     },
