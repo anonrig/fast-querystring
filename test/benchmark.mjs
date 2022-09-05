212c51ce68c4109de4ba1c798bd8008ed17cce9e
Yagiz Nizipli <yagiz@nizipli.com>
2022-09-05 13:08:07 -0400

    perf: update URLSearchParams benchmark to reflect real-world usage

--- test/benchmark.mjs
+++ test/benchmark.mjs
@@ -7,19 +7,46 @@ import queryString from "query-string";
 await benchmark(
   {
     qs() {
-      return qs.parse("frappucino=muffin&goat%5B%5D=scone&pond=moose");
+      return qs.parse(
+        "frappucino=muffin&goat=scone&pond=moose&foo=bar&foo=baz",
+      );
     },
     "fast-querystring"() {
-      return fastQueryString.parse("frappucino=muffin&goat%5B%5D=scone&pond=moose");
+      return fastQueryString.parse(
+        "frappucino=muffin&goat=scone&pond=moose&foo=bar&foo=baz",
+      );
     },
     "node:querystring"() {
-      return native.parse("frappucino=muffin&goat%5B%5D=scone&pond=moose");
+      return native.parse(
+        "frappucino=muffin&goat=scone&pond=moose&foo=bar&foo=baz",
+      );
     },
     "query-string"() {
-      return queryString.parse("frappucino=muffin&goat%5B%5D=scone&pond=moose");
+      return queryString.parse(
+        "frappucino=muffin&goat=scone&pond=moose&foo=bar&foo=baz",
+      );
     },
-    URLSearchParams() {
-      return new URLSearchParams("frappucino=muffin&goat%5B%5D=scone&pond=moose");
+    "URLSearchParams-with-Object.fromEntries"() {
+      const urlParams = new URLSearchParams(
+        "frappucino=muffin&goat=scone&pond=moose&foo=bar&foo=baz",
+      );
+      return Object.fromEntries(urlParams);
+    },
+    "URLSearchParams-with-construct"() {
+      const u = new URLSearchParams(
+        "frappucino=muffin&goat=scone&pond=moose&foo=bar&foo=baz",
+      );
+      const data = {};
+      for (const [key, value] of u.entries()) {
+        if (Array.isArray(data[key])) {
+          data[key].push(value);
+        } else if (data[key]) {
+          data[key] = [].concat(data[key], value);
+        } else {
+          data[key] = value;
+        }
+      }
+      return data;
     },
   },
   { warmup: true, print: { compare: true, compareMode: "previous" } },
