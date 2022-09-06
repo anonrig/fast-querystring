f517380dd068ad58d7f54b8a3f5b2964675b2b0c
Yagiz Nizipli <yagiz@nizipli.com>
2022-09-06 19:11:35 -0400

    fix: handle exported function definitions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,4 +3,6 @@ declare namespace FastQuerystring {
   export function parse(value: string): Record<string, any>;
 }
 
+export function stringify(value: Record<string, any>): string;
+export function parse(value: string): Record<string, any>;
 export default FastQuerystring;
