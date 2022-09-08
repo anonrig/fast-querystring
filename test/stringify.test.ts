597be15034ad4adc923b45f542b84962733bee18
Yagiz Nizipli <yagiz@nizipli.com>
2022-09-08 16:56:46 -0400

    test: add tests to stringify for validation

--- test/stringify.test.ts
+++ test/stringify.test.ts
@@ -1,6 +1,7 @@
 import { qsNoMungeTestCases, qsTestCases } from "./node";
 import qs from "../lib";
 import { test, assert } from "vitest";
+import querystring from "querystring";
 
 test("should stringify the basics", () => {
   qsNoMungeTestCases.forEach((t) => {
@@ -12,4 +13,13 @@ test("should succeed on node.js tests", () => {
   qsTestCases.forEach((t) => {
     assert.deepEqual(qs.stringify(t[2]), t[1]);
   });
+});
+
+test("native querystring module should match the test suite result", () => {
+  qsTestCases.forEach(
+    (t) => assert.deepEqual(querystring.stringify(t[2]), t[1]),
+  );
+  qsNoMungeTestCases.forEach(
+    (t) => assert.deepEqual(querystring.stringify(t[1]), t[0]),
+  );
 });
