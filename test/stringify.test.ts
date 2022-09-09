8157b750bc52ee37f56869cac0ae2b7e3930b0d9
Yagiz Nizipli <yagiz@nizipli.com>
2022-09-09 08:37:58 -0400

    test: improve code coverage

--- test/stringify.test.ts
+++ test/stringify.test.ts
@@ -16,3 +16,35 @@ test("native querystring module should match the test suite result", () => {
     (t) => assert.deepEqual(querystring.stringify(t[1]), t[0]),
   );
 });
+
+test("should handle numbers", () => {
+  assert.deepEqual(
+    qs.stringify({ age: 5, name: "John Doe" }),
+    "age=5&name=John%20Doe",
+  );
+});
+
+test("should handle BigInt", () => {
+  assert.deepEqual(
+    qs.stringify({ age: BigInt(55), name: "John" }),
+    "age=55&name=John",
+  );
+});
+
+test("should handle boolean values", () => {
+  assert.deepEqual(qs.stringify({ valid: true }), "valid=true");
+  assert.deepEqual(qs.stringify({ valid: false }), "valid=false");
+});
+
+test("should handle numbers", () => {
+  assert.deepEqual(qs.stringify({ value: 1e22 }), "value=1e%2B22");
+});
+
+test("should omit objects", () => {
+  // This aligns with querystring module
+  assert.deepEqual(qs.stringify({ user: {} }), "user=");
+});
+
+test("should omit non-object inputs", () => {
+  assert.deepEqual(qs.stringify("hello" as any), "");
+});
