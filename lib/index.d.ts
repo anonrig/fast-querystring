3ca7a5f6d95d6e07c9d96a0f34cdc92d1423bee2
uzlopak <aras.abbasi@googlemail.com>
2023-01-04 10:51:04 +0100

    test typescript interop

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,14 @@
-declare namespace FastQuerystring {
+type FastQueryString = {
+  stringify(value: Record<string, any>): string;
+  parse(value: string): Record<string, any>;
+};
+
+declare namespace fastQueryString {
   export function stringify(value: Record<string, any>): string;
   export function parse(value: string): Record<string, any>;
+
+  const fqs: FastQueryString;
+  export { fqs as default };
 }
 
-export function stringify(value: Record<string, any>): string;
-export function parse(value: string): Record<string, any>;
-export default FastQuerystring;
+export = fastQueryString;
